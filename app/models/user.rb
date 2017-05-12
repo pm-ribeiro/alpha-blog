cb5a93f9c4087f771b43a2f054271170ed48281e
Patricia Ribeiro <pmribeiro@inf.ufpel.edu.br>
2017-05-12 01:00:20 +0000

    add association between user and article, and debufger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true,uniqueness: { case_sensitive:false}, 
   length: { minimum: 3, maximum: 25 }
   
@@ -6,5 +9,6 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: { case_sensitive:false}, 
   length: { minimum: 3, maximum: 105 }, format: {with: VALID_EMAIL_REGEX}
+  
   
 end
