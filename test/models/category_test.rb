895eb0b4006523608da787261ecdc472e7d1a443
Patricia Ribeiro <pmribeiro@inf.ufpel.edu.br>
2017-05-25 12:39:58 +0000

    add categories model and test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,18 +7,18 @@ class CategoryTest < ActiveSupport::TestCase
   end
   
   test "category should be valid" do
-    assert @category.valid?
+   assert @category.valid?
   end
   
   test "name should be present" do
-    @category.name = " "
-    assert_not @category.valid?
+   @category.name = " "
+   assert_not @category.valid?
   end
   
   test "name should be unique" do 
-    @category.save
-    category2 = Category.new(name: "sports")
-    assert_not category2.valid?
+   @category.save
+   category2 = Category.new(name: "sports")
+   assert_not category2.valid?
   end
   
   test "name should not be too long" do
@@ -29,8 +29,6 @@ class CategoryTest < ActiveSupport::TestCase
   test "name should not be too short" do
     @category.name = "aa"
     assert_not @category.valid?
-  end
-  
+  end  
+ 
 end
-
-
